fb03bd7988cbabf22dd478b3dc46f8aa4caacd4a
mayhamri <mayhamri@tau.mail.ac.il>
2021-12-04 18:11:39 +0200

    project AVL. Insert and join and split debugged delete has bug

--- src/projectAVL.java
+++ src/projectAVL.java
@@ -1,57 +1,109 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-
+import java.util.Random;
 public class projectAVL {
+    public static int numOfPairs(ArrayList<Integer> arr){
+        int sum = 0;
+        for(int i = 0; i< arr.size() ; i ++){
+            for (int j = 0 ; j< i ; j ++){
+                if (arr.get(j)>arr.get(i)){
+                    sum ++;
+                }
+            }
+        }
+        return sum;
+
+    }
+
+    public static int getDistFromMax(AVLTree t, AVLTree.IAVLNode node1){
+        AVLTree.IAVLNode root = t.getRoot();
+        AVLTree.IAVLNode max = t.getMaxIAVL();
+        while(node1.getKey() >root.getKey() ){
+            if((root.getRight() != null)&&(root.getRight().isRealNode())){
+                root = root.getRight();
+            }
+            else{
+                break;
+            }
+        }
+        int path = BinarySearch(root,node1)+BinarySearch(root,max) +1 ;
+
+        return path;
+    }
+
+    public static int BinarySearch(AVLTree.IAVLNode root, AVLTree.IAVLNode node1){
+        int cnt = 0 ;
+        while ( root!= node1){
+            if(root.getKey()>node1.getKey()){
+                cnt +=1;
+                root = root.getLeft();
+            }
+            else{
+                cnt +=1;
+                root = root.getRight();
+            }
+        }
+        return cnt;
+    }
+
     public static void main(String[] args){
-        AVLTree [] trees = new AVLTree[20];
+        AVLTree [] trees = new AVLTree[10];
         for( int i = 0;i < trees.length; i ++){
            trees[i] = new AVLTree();
         }
 
+        AVLTree tr = new AVLTree();
+        for ( int i = 0 ; i < 5 ; i ++ ){
+            tr.insert(i , "maaaa ");
+        }
+        int lol = getDistFromMax(tr,tr.getNodeBykey(0));
+        int lol2 = getDistFromMax(tr,tr.getNodeBykey(2));
+        if ( (lol2!= 3)||(lol!=4)){
+            System.out.println("erororrrrrrr");
+        }
 
-        for (int i =0 ; i < 10 ; i+=1 ){
+
+        for (int i =0 ; i < 5 ; i+=1 ){
             int n = (int)(1000*(Math.pow(2,i+1)));
             ArrayList<Integer> list = new ArrayList<Integer>();
             for (int j = 0; j < n ; j ++){
                 list.add(j);
             }
-            Collections.shuffle(list);
-            for ( int j = 0; j< n ; j++ ){
-                trees[i].insert(list.get(j),"tom" );
-                trees[i+10].insert(list.get(j),"may" );
+            Collections.reverse(list);
+            int firstnum = numOfPairs(list);
+            trees[i].insert(list.get(0), "tom");
+            int cnt= 0;
+            for ( int j = 1; j< n ; j++ ) {
+                AVLTree.IAVLNode node1 = trees[i].getNodeBykey(list.get(j));
+                int num = getDistFromMax(trees[i],node1);
+                cnt += num;
+                trees[i].insert(list.get(j),"tom");
             }
+            System.out.println("search cost first "+ cnt);
+
+
+
+
+            Collections.shuffle(list);
+            int secnum = numOfPairs(list);
+            trees[i+5].insert(list.get(0), "tom");
+            cnt= 0;
+            for ( int j = 1; j< n ; j++ ) {
+                AVLTree.IAVLNode node1 = trees[i+5].getNodeBykey(list.get(j));
+                int num = getDistFromMax(trees[i+5],node1);
+                cnt += num;
+                trees[i+5].insert(list.get(j),"tom");
+            }
+            System.out.println("search cost second "+ cnt);
         }
-        //test BF
-        for (int i = 0 ; i < 10 ; i ++){
-            trees[i].testRanks(trees[i].getRoot());
-        }
 
 
-        int num = (int)(1000*(Math.pow(2,1)));
-        AVLTree testtree = new AVLTree();
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        for (int j = 0; j< num*2 ; j++){
-            list2.add(j + 2 + num);
-        }
-        Collections.shuffle(list2);
-        for ( int j = 0; j< num*2 ; j++){
-            testtree.insert(list2.get(j),"tom" );
-            testtree.insert(list2.get(j),"may" );
-        }
-        AVLTree.IAVLNode x = new AVLTree.AVLNode("LOL", num+1);
-
-        AVLTree [] ans = testtree.split(4174);
-        ans[0].testRanks(ans[0].getRoot());
-        ans[1].testRanks(ans[1].getRoot());
 
 
-      //  for (int i = 0 ; i < num ; i +=3){
-        //    trees[0].delete(i);
-      //  }
-      //  System.out.println("all goof insert");
-     //   trees[0].testRanks(trees[0].getRoot());
-      //  AVLTree.IAVLNode p = trees[0].getNodeBykey(919);
+
+
+
 
 
 
